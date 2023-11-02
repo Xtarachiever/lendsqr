f682893cb565384daadd8aaeaeec544fd60fa708
Xtarachiever <estheradeyemi796@gmail.com>
2023-11-02 11:54:17 -0700

    Updated the navbar

--- lendsqr/src/components/layout/NavBar.tsx
+++ lendsqr/src/components/layout/NavBar.tsx
@@ -3,12 +3,15 @@ import TextInput from '../reuseableFields/TextInput';
 import { BsSearch } from 'react-icons/bs';
 import { IoIosNotificationsOutline } from 'react-icons/io';
 import { RiArrowDownSFill } from 'react-icons/ri';
+import { AiOutlineClose } from 'react-icons/ai';
 import './styles.scss';
+import { useState } from 'react';
 
 interface INavBarProps {
 }
 
 const NavBar: React.FunctionComponent<INavBarProps> = (props) => {
+    const [open, setOpen] = useState<boolean>(false)
   return (
     <div className='nav-wrapper'>
         <img src='/Logo.svg' alt='Logo'/>
@@ -25,6 +28,22 @@ const NavBar: React.FunctionComponent<INavBarProps> = (props) => {
                 <img src='/user-icon.svg' alt='profile'/>
                 <p>Adedeji <RiArrowDownSFill /></p>
             </div>
+        </div>
+        <div className='profile mobile-view'>
+            <img src='/user-icon.svg' alt='profile' onClick={()=>setOpen(true)}/>
+            {
+                open && 
+                <div className='drop-down'>
+                    <div className='close-button' onClick={()=>setOpen(false)}>
+                        <AiOutlineClose color='#213F7D'/>
+                    </div>
+                    <div className='dropdown-content'>
+                        <IoIosNotificationsOutline fontSize={'1.4rem'}/>
+                        {/* <p>Notifications <IoIosNotificationsOutline fontSize={'1.4rem'}/></p> */}
+                        <p className='docs'>Docs</p>
+                    </div>
+                </div>
+            }
         </div>
     </div>
   );
